7fb40c3bcdaf81fcd7a5349e4f8136508aa750fa
kaguya0209 <k016c1308@it-neec.jp>
2019-01-08 14:25:30 +0900

    searchTopのデータを受け取ってresultに表示する機能を追加（未完成)

--- logic/searchtop.py
+++ logic/searchtop.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request, redirect, url_for
 
-searchtop = Blueprint('searchtop', __name__,template_folder='templates',static_folder='./static')
+search = Blueprint('search', __name__,template_folder='templates',static_folder='./static')
 
-@searchtop.route('/search')
-def hello():
+@search.route('/searchtop',methods=['GET','POST'])
+def searchtop():
     return render_template('searchTop.html')
+
+@search.route('/searchresult',methods=['GET','POST'])
+def result():
+    if request.method == 'POST':
+        locate = request.form['locate']
+        checkin = request.form['checkIn']
+        checkout = request.form['checkOut']
+        roomnumber = request.form['roomnumber']
+        adult = request.form['adult']
+        child = request.form['child']
+        money = request.form['range']
+        return render_template('SearchResult.html',locate=locate,checkIn=checkin,checkOut=checkout,roomnumber=roomnumber,adult=adult,child=child,range=money)
