0c66a976b07896d0f8df352c68b402c9cc99e902
kaguya0209 <k016c1308@it-neec.jp>
2019-01-08 09:49:47 +0900

    ひな形の作成

---
+++ hoterusi.py
@@ -0,0 +1,12 @@
+from flask import flask,render_template,request, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('Toppage.html')
+
+
+if __name__ == '__main__':
+    app.run()
