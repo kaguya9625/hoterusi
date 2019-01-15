2eaedcc97a3cebcc16dc8180ba5e9189fa1d6eef
kaguya0209 <k016c1308@it-neec.jp>
2019-01-15 14:21:45 +0900

    エラー時の対応を追加

--- logic/Search.py
+++ logic/Search.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template,request, redirect, url_for
+from flask import Blueprint, render_template,request, redirect, url_for, flash
 from tkinter import messagebox
+import sqlite3
 
 search = Blueprint('search', __name__,template_folder='templates',static_folder='./static')
 
 #検索画面Topを表示
 @search.route('/searchtop',methods=['GET','POST'])
 def searchtop():
-    return render_template('searchTop.html')
+    return render_template('SearchTop.html')
 #検索結果を表示
 @search.route('/searchresult',methods=['GET','POST'])
 def result():
@@ -19,9 +20,16 @@ def result():
         adult = request.form['adult']
         child = request.form['child']
         money = request.form['range']
-        searchconditions = '場所 ' + locate + '日時 ' + checkin +' ~ '+ checkout + '客室数 ' +roomnumber + '大人:' + adult + '子供:'+child + '予算' + money + '万円'
-        # if locate == '' or checkin == '' or checkout == '' or roomnumber == '' or adult == '' or child =='' or money == '':
-        #         messagebox.showerror('エラー', '必須事項が記入されていません')
-        return render_template('SearchResult.html',locate=locate,CheckIn=checkin,CheckOut=checkout,roomnumber=roomnumber,adult=adult,child=child,range=money,searchconditions=searchconditions)
+        searchconditions = '場所 ' + locate + '日時 ' + checkin +' ~ '+ checkout + '客室数 ' + roomnumber +'部屋' + '大人:' + adult +'人'+ '子供:'+child +'人'+ '予算' + money + '万円'
+        if locate == '' or checkin == '' or checkout == '' or roomnumber == '' or adult == '' or child == '' or money == '':
+            return render_template('searcherror.html',msg='記入されてない事項があります')
+        else:
+            if checkin < checkout: 
+                return render_template('SearchResult.html',locate=locate,CheckIn=checkin,CheckOut=checkout,roomnumber=roomnumber,adult=adult,child=child,range=money,searchconditions=searchconditions)
+            else:
+                return render_template('searcherror.html',msg='誤った情報があります。')    
     else:
-        return render_template('searchTop.html')
+        return render_template('SearchTop.html')
+
+
+
