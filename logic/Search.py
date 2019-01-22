b900ea84723b4d28ec82ccd72332fa1b2036400b
kaguya0209 <k016c1308@it-neec.jp>
2019-01-22 13:09:56 +0900

    ファイルの更新

--- logic/Search.py
+++ logic/Search.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template,request, redirect, url_for, flash
-from tkinter import messagebox
 import sqlite3
+from logic import func
 
 search = Blueprint('search', __name__,template_folder='templates',static_folder='./static')
+
 
 #検索画面Topを表示
 @search.route('/searchtop',methods=['GET','POST'])
@@ -12,7 +13,7 @@ def searchtop():
 @search.route('/searchresult',methods=['GET','POST'])
 def result():
     if request.method == 'POST':
-            #検索条件を取得
+        #検索条件を取得
         locate = request.form['locate']
         checkin = request.form['CheckIn']
         checkout = request.form['CheckOut']
@@ -20,12 +21,18 @@ def result():
         adult = request.form['adult']
         child = request.form['child']
         money = request.form['range']
-        searchconditions = '場所 ' + locate + '日時 ' + checkin +' ~ '+ checkout + '客室数 ' + roomnumber +'部屋' + '大人:' + adult +'人'+ '子供:'+child +'人'+ '予算' + money + '万円'
+        #検索条件の表示のための文章生成
+        searchconditions = '場所 ' +''+ locate + '日時 ' + checkin +' ~ '+ checkout + '客室数 ' + roomnumber +'部屋' + '大人:' + adult +'人'+ '子供:'+child +'人'+ '予算' + money + '万円'
+        #非記入欄があった場合errorを吐き searcherror画面に遷移。
         if locate == '' or checkin == '' or checkout == '' or roomnumber == '' or adult == '' or child == '' or money == '':
             return render_template('searcherror.html',msg='記入されてない事項があります')
         else:
+            #チェックイン日がチェックアウト日より小さい場合検索条件を検索結果画面に渡す。
             if checkin < checkout: 
+                hotel = func.searchhotel(locate)
+                print(type(hotel))
                 return render_template('SearchResult.html',locate=locate,CheckIn=checkin,CheckOut=checkout,roomnumber=roomnumber,adult=adult,child=child,range=money,searchconditions=searchconditions)
+            #チェックイン日がチェックアウト日より大きい場合searcherro画面に遷移
             else:
                 return render_template('searcherror.html',msg='誤った情報があります。')    
     else:
--- logic/func.py
+++ logic/func.py
@@ -1,6 +1,19 @@
 import sqlite3
 
-def adduser():
+#ホテルを検索するsql文の実行
+#引数 検索時の地名
+#0件の場合 msg（ホテルがありません)と返す
+#それ以外の場合配列で結果を返す
+def searchhotel(locate):
+    msg = 'ホテルがありません'
     conn = sqlite3.connect('hoterusi.db')
     c = conn.cursor()
-    c.excute
+    sql = 'select * from hotel where hotel_addres like ?;'
+    c.execute(sql,('%'+ locate,+'%'))
+    result = c.fetchall()
+    if result == '':
+        return msg
+    else:
+        return result
+    c.close()
+    conn.close()
