b8bdb0600c8877c496a66046beda2e5522d0194e
kaguya0209 <k016c1308@it-neec.jp>
2019-01-29 14:27:06 +0900

    関数の追加及び変更

--- logic/func.py
+++ logic/func.py
@@ -1,19 +1,51 @@
 import sqlite3
 
+#sql文を実行する関数
+#第一引数にsql文、第二引数にparamを渡す
+def paramconnect(sql,param):
+    conn = sqlite3.connect('hoterusi/hoterusi.db')
+    c = conn.cursor()
+    c.execute(sql,param)
+    result = c.fetchall()
+    conn.close()
+    return result
+    
+#sql文を実行する関数
+#sql文を渡すのみ
+def dbconnect(sql):
+    conn = sqlite3.connect('hoterusi/hoterusi.db')
+    c = conn.cursor()
+    c.execute(sql)
+    result = c.fetchall()
+    conn.close()
+    return result
+
+
 #ホテルを検索するsql文の実行
 #引数 検索時の地名
 #0件の場合 msg（ホテルがありません)と返す
 #それ以外の場合配列で結果を返す
 def searchhotel(locate):
     msg = 'ホテルがありません'
-    conn = sqlite3.connect('hoterusi.db')
-    c = conn.cursor()
-    sql = "select * from hotel where hotel_address like '%?%',(word,)"
-    c.execute("select * from hotel where hotel_address like ?;",(locate+'%',))
-    result = c.fetchall()
+    sql = 'select * from hotel where hotel_address like ?'
+    locate1 = (locate + '%',) 
+    result = paramconnect(sql,locate1)
     if result == '':
         return msg
     else:
         return result
-    c.close()
-    conn.close()
+#ホテルのルーム情報を取得する関数
+#引数hotelid
+def selecthotel(hotelid):
+    sql = 'select * from room where hotel_id like ?'
+    result = paramconnect(sql,hotelid)
+    return result
+
+#ルームタイプを取得する関数
+#引数無し
+def roomdata():
+    conn = sqlite3.connect('hoterusi.db')
+    c = conn.cursor()
+    sql = 'select * from room_type'
+    result = dbconnect(sql)
+    return result
