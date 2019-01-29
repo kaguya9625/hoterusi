038f577e7a1db484aa138d812b655a835134a611
kaguya0209 <k016c1308@it-neec.jp>
2019-01-29 14:26:17 +0900

    各ホテルのデータごとの部屋タイプや値段が出るように実装

--- logic/Roomchoice.py
+++ logic/Roomchoice.py
@@ -1,7 +1,27 @@
 from flask import Blueprint, render_template,request, redirect, url_for
+from logic import func
 
 choice = Blueprint('choice', __name__,template_folder='templates',static_folder='./static')
 
-@choice.route('/Roomchoice')
+@choice.route('/Roomchoice',methods=['GET','POST'])
 def Roomchoice():
-    return render_template('Roomchoice.html')
+    if request.method == 'POST':
+        hotelid = request.form['hotel']
+        price = func.selecthotel(hotelid)
+        roomtype = func.roomdata()
+        roomlist = []
+        for item in price:
+            if item[1] == 1:
+                roomlist.append('シングル')
+            if item[1] == 2:
+                roomlist.append('ツイン')
+            if item[1] == 3:
+                roomlist.append('ダブル')
+        list = []
+        for room, type in zip(roomlist, price):
+            setlist = [room,type]
+            list.append([setlist[0],setlist[1][2],setlist[1][1]])
+
+        return render_template('Roomchoice.html',hotelinfo = list,info = price)
+    else:
+        return render_template('Roomchoice.html')
