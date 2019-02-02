73a999e66bf428d93404975170b646f9eb4b6316
rusian0 <sssdaphne@gmail.com>
2019-02-02 12:43:09 +0900

    キャンセル確認画面

--- logic/reservation_cancel.py
+++ logic/reservation_cancel.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, render_template,request, redirect, url_for
+from flask import Blueprint, render_template,request, redirect, url_for, session
+from logic import func
+import ast
 
 re_can = Blueprint('re_can', __name__,template_folder='templates',static_folder='./static')
 
-@re_can.route('/reservation_cancel')
+@re_can.route('/reservation_cancel',  methods=['GET', 'POST'])
 def reservation_cancel():
-    return render_template('reservation_cancel.html')
+    if request.method == 'POST':
+        reser = request.form['reser']
+        reser = ast.literal_eval(reser)
+    return render_template('reservation_cancel.html',re = reser)
+
+# @re_con.route('/reservation_cancel/confirm',  methods=['GET', 'POST'])
+# def reservation_confirm():
+#     if request.method == 'POST':
+#         reser = request.form['reser']
+#         reser = ast.literal_eval(reser)
+#     # return render_template('reservation_cancel.html',re = reser)
+#     return "a"
